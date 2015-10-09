ae62f9346968e444e922b2ef8d82dc3d7f5e4416
Olivelli Putelli <olivelliputelli@live.it>
2015-10-09 10:16:44 +0200

    Aggiunta classe Automobile  in Program

--- Classi_5_AINT/Classi_5_AINT/Program.cs
+++ Classi_5_AINT/Classi_5_AINT/Program.cs
@@ -6,21 +6,40 @@ using System.Threading.Tasks;
 
 namespace Classi_5_AINT
 {
+    enum Colori {blue,nero,giallo,bianco};
+
+    class Automobile
+    {
+        public string Produttore { get; set; }
+        public string Modello { get; set; }
+        public int Anno { get; set; }
+        public Colori Colore { get; set; }
+
+        public double ValoreDiMercaro()
+        {
+            //Codice e codice....
+            return 50.0F - (DateTime.Now.Year - this.Anno);
+        }
+      
+    }
     class Program
     {
+       
         static void Main(string[] args)
         {
-            Cilindro cl1 = new Cilindro(3.0F,4.0F);
-            Cerchio c1 = new Cerchio(4.0F);
+            Automobile miaAuto = new Automobile();
+            miaAuto.Produttore = "Fiat";
+            miaAuto.Modello = "Punto 1300cc";
+            miaAuto.Anno = 1990;
+            miaAuto.Colore = Colori.blue;
 
-            Console.WriteLine(c1);
-            Console.WriteLine(cl1);
 
-            Cerchio c2 = c1;
-            c2 = null;
-            c1 = null;
+
+            Console.WriteLine(miaAuto.ValoreDiMercaro());
 
             Console.ReadKey();
         }
+
+
     }
 }
